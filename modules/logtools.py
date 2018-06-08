1ef49bb97679ac91d85e4288801962071ab63443
znuxor <znuxor@gmail.com>
2018-06-08 05:41:41 -0400

    pycodestyle + pylint + flake8 on logtools.py

--- modules/logtools.py
+++ modules/logtools.py
@@ -2,21 +2,17 @@
 '''Stuff about making the spreadsheet information a bit more useful'''
 
 import shlex
-import re
-import os
 import argparse
-import subprocess
-import urllib
 import time
 import requests
-from sopel import module
-import sopel.tools
-from sopel.config.types import StaticSection, ListAttribute, ValidatedAttribute, FilenameAttribute, NO_DEFAULT
 from apiclient.discovery import build
 from fuzzywuzzy import fuzz
+from sopel import module
+from sopel.config.types import StaticSection, ListAttribute, ValidatedAttribute
 
 
 class LogToolsSection(StaticSection):
+    '''Data class containing the parameters for the module.'''
     google_api_key_password = ValidatedAttribute('google_api_key_password')
     admin_channels = ListAttribute('admin_channels')
     acceptable_fuzz_ratio = ValidatedAttribute('acceptable_fuzz_ratio', int, default=75)
@@ -24,30 +20,38 @@ class LogToolsSection(StaticSection):
     relevant_sheets = ListAttribute('relevant_sheets')
     relevant_range = ValidatedAttribute('relevant_range')
 
+
 def configure(config):
+    '''Invoked by the configuration building mode of sopel.'''
     config.define_section('logtools', LogToolsSection, validate=True)
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('term', type=str, help='the nick or piece of mask to search')
 
+
 def setup(bot):
+    '''Invoked when the module is loaded.'''
     bot.config.define_section('logtools', LogToolsSection, validate=True)
-    bot.memory['google_sheets_service'] = build('sheets', 'v4', developerKey=bot.config.logtools.google_api_key_password, cache_discovery=False)
+    bot.memory['google_sheets_service'] = \
+        build('sheets', 'v4',
+              developerKey=bot.config.logtools.google_api_key_password,
+              cache_discovery=False)
     if 'sheet_content_1' in bot.memory:
         del bot.memory['sheet_content_1']
     if 'sheet_content_2' in bot.memory:
         del bot.memory['sheet_content_2']
 
+
 ACCEPTABLE_RATIO = 75
+
 
 @module.commands('search')
 def search(bot, trigger):
+    '''Searches for a nick (fuzzy) for a part of a netmask in the spreadsheets.'''
 
-    extra_info = ''
-
-    isAdminChannel = (trigger.sender in bot.config.logtools.admin_channels)
-    if not isAdminChannel:
+    is_admin_channel = (trigger.sender in bot.config.logtools.admin_channels)
+    if not is_admin_channel:
         return
 
     arguments = trigger.groups()[1]
@@ -56,7 +60,7 @@ def search(bot, trigger):
         return
     try:
         args = parser.parse_args(shlex.split(arguments))
-    except SystemExit as e:
+    except SystemExit:
         if '-h' in arguments or '--help' in arguments:
             helpsearch(bot, trigger)
         else:
@@ -70,21 +74,31 @@ def search(bot, trigger):
     found_indexes_2 = []
 
     for index, line in enumerate(bot.memory['sheet_content_1']):
-        if line and (args.term in line[8] or fuzz.ratio(args.term.lower(), line[1].lower()) >= ACCEPTABLE_RATIO):
+        if line and (args.term in line[8] or fuzz.ratio(args.term.lower(),
+                                                        line[1].lower()) >= ACCEPTABLE_RATIO):
             found_indexes_1.append(index)
 
     for index, line in enumerate(bot.memory['sheet_content_2']):
-        if line and (args.term in line[8] or fuzz.ratio(args.term.lower(), line[1].lower()) >= ACCEPTABLE_RATIO):
+        if line and (args.term in line[8] or fuzz.ratio(args.term.lower(),
+                                                        line[1].lower()) >= ACCEPTABLE_RATIO):
             found_indexes_2.append(index)
 
     sheet_1_instances = []
     sheet_2_instances = []
     for an_index in found_indexes_1:
         relevant_row = bot.memory['sheet_content_1'][an_index]
-        sheet_1_instances.append('{} on {} ({}) (row {})'.format(relevant_row[2], relevant_row[1], relevant_row[8], an_index+1+1))  # one for 1-index, one for missing row
+        report_str = '{} on {} ({}) (row {})'.format(relevant_row[2],
+                                                     relevant_row[1],
+                                                     relevant_row[8],
+                                                     an_index+1+1)  # for index and missing row
+        sheet_1_instances.append(report_str)
     for an_index in found_indexes_2:
         relevant_row = bot.memory['sheet_content_2'][an_index]
-        sheet_2_instances.append('{} on {} ({}) (row {} (old sheet))'.format(relevant_row[2], relevant_row[1], relevant_row[8], an_index+1+1))
+        report_str = '{} on {} ({}) (row {} (old sheet))'.format(relevant_row[2],
+                                                                 relevant_row[1],
+                                                                 relevant_row[8],
+                                                                 an_index+1+1)
+        sheet_2_instances.append(report_str)
 
     instances = sheet_1_instances + sheet_2_instances
 
@@ -96,24 +110,36 @@ def search(bot, trigger):
 
 RELEVANT_SHEETS = ('Operator Actions', 'Pre 2018 Data')
 RELEVANT_RANGE = 'a2:k'
+
+
 @module.interval(300)
 def refresh_spreadsheet_content(bot):
-    bot.memory['sheet_content_1'] = bot.memory['google_sheets_service'].spreadsheets().values().get(spreadsheetId=bot.config.logtools.spreadsheet_id, range=RELEVANT_SHEETS[0]+'!'+RELEVANT_RANGE).execute().get('values', [])
-    time.sleep(1)
-    bot.memory['sheet_content_2'] = bot.memory['google_sheets_service'].spreadsheets().values().get(spreadsheetId=bot.config.logtools.spreadsheet_id, range=RELEVANT_SHEETS[1]+'!'+RELEVANT_RANGE).execute().get('values', [])
+    '''Periodically refreshes the spreadsheet content.
+    This is done this way to limits calls to the API.'''
 
+    values_obj = bot.memory['google_sheets_service'].spreadsheets().values()
+
+    range_1 = RELEVANT_SHEETS[0]+'!'+RELEVANT_RANGE
+    range_2 = RELEVANT_SHEETS[1]+'!'+RELEVANT_RANGE
+    bot.memory['sheet_content_1'] = \
+        values_obj.get(spreadsheetId=bot.config.logtools.spreadsheet_id,
+                       range=range_1).execute().get('values', [])
+    time.sleep(1)
+    bot.memory['sheet_content_2'] = \
+        values_obj.get(spreadsheetId=bot.config.logtools.spreadsheet_id,
+                       range=range_2).execute().get('values', [])
 
 
 @module.commands('helpsearch')
 def helpsearch(bot, trigger):
-    isAdminChannel = (trigger.sender in bot.config.logtools.admin_channels)
-    if not isAdminChannel:
+    '''Serves the help documentation.'''
+    is_admin_channel = (trigger.sender in bot.config.logtools.admin_channels)
+    if not is_admin_channel:
         return
     help_content = parser.format_help()
     help_content = help_content.replace('sopel', ',search')
     url = create_snoonet_paste(help_content)
     bot.reply(url)
-
 
 
 def create_snoonet_paste(paste_content):
@@ -130,6 +156,4 @@ def create_snoonet_paste(paste_content):
     # We create a post
     response = requests.post(post_url, data=data)
 
-    #return paste_url_prefix + url_unique_code
     return response.url
-
